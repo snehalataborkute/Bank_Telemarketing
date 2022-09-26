a0a1533bda7e2897c2661bedf98a01e8e42a91e8
Snehalata Borkute <snehalatab03@gmail.com>
2022-09-26 14:27:11 +0530

    added modified files

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ import os
 
 PORT_NUMBER = 5001
 
-MODEL_FILE_PATH = os.path.join(r'D:\VELOCITY_DATA\Project\Bank_telemarketing_Project',r'articrafts\RF_Model.pkl')
+MODEL_FILE_PATH = os.path.join('articrafts','RF_Model.pkl')
 
-JSON_FILE_PATH = os.path.join(r'D:\VELOCITY_DATA\Project\Bank_telemarketing_Project',r'articrafts\Project_Data_feature.json')
+JSON_FILE_PATH = os.path.join('articrafts','Project_Data_feature.json')
 
-LABEL_ENCODED_DATA = os.path.join(r'D:\VELOCITY_DATA\Project\Bank_telemarketing_Project',r'articrafts\Label_Encoded_Data.pkl')
+LABEL_ENCODED_DATA = os.path.join('articrafts','Label_Encoded_Data.pkl')
+
+
+
+
+
